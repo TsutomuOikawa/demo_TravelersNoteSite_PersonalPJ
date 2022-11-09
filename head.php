67f852f3e69cf544242c30566431e9f17df28959
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-11-09 10:31:33 +0900

    ヘッド・ヘッダー

--- head.php
+++ head.php
@@ -2,6 +2,9 @@
 <html lang="ja">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $page_title.'｜Globe Note'; ?></title>
+    <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="icon/css/all.css">
   </head>
--- header.php
+++ header.php
@@ -5,11 +5,12 @@
       <form class="header_form" action="index.html" method="post">
         <input type="text" name="" class="header_input" value="" placeholder="都道府県名を入力">
         <input type="text" name="" class="header_input" value="" placeholder="キーワードを入力">
+        <button type="submit" class="header_submit" name=""><i class="fas fa-search fa-lg"></i></button>
       </form>
       <ul class="header_menu">
-        <li class="header_menuItem"><a href="#" class="link header_menuItem_link">About</a></li>
+        <li class="header_menuItem"><a href="index.php#about" class="link header_menuItem_link">About</a></li>
         <li class="header_menuItem"><a href="#" class="link header_menuItem_link">Sign in</a></li>
-        <li class="header_menuItem strongPoint"><a href="#" class="link header_menuItem_link header_menuItem_link--strong">Register</a></li>
+        <li class="header_menuItem"><a href="#" class="link header_menuItem_link">Register</a></li>
       </ul>
     </div>
   </header>
