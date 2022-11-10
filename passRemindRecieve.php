65d8d4c4de1185e3dcc2ed5df0d2b7a0313f11f1
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-11-10 15:45:09 +0900

    create passReminder page

---
+++ passRemindRecieve.php
@@ -0,0 +1,36 @@
+
+<?php
+$page_title = 'パスワード再発行';
+require('head.php');
+require('header.php');
+?>
+<div class="alert--err js-show-err-msg">
+  <p><?php //echo $err_msg['common']; ?></p>
+</div>
+
+<main class="page-wrapper">
+  <div class="container--baseColor">
+    <h1 class="container_title">パスワード再発行手続き</h1>
+    <p>メールアドレスに届いた認証コードを入力してください<br>パスワードを再発行し、再度メールをお送りします</p>
+    <div class="container_body--xs">
+      <form method="post" class="form">
+      <h2 class="form_title">認証コードを入力してください</h2>
+
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            認証コード
+          </div>
+          <input type="text" name="auth_key" class="form_input" value="" placeholder="">
+        </label>
+        <p class="form_errMsg"></p>
+
+        <button type="submit" class="form_button" name="">認証する</button>
+        <p class="form_notion"><a href="login.php" class="link"><span style="font-size:18px;">&gt</span> ログインフォームへ戻る</a></p>
+      </form>
+    </div>
+  </div>
+</main>
+
+<!--　共通フッター呼び出し　-->
+<?php require('footer.php'); ?>
---
+++ passRemindSend.php
@@ -0,0 +1,39 @@
+
+<?php
+$page_title = '認証コード送信';
+require('head.php');
+require('header.php');
+?>
+<div class="alert--err js-show-err-msg">
+  <p><?php //echo $err_msg['common']; ?></p>
+</div>
+
+<main class="page-wrapper">
+  <div class="container--baseColor">
+    <h1 class="container_title">パスワード再発行手続き</h1>
+    <p>ご登録のメールアドレス宛に認証コードをお送りします</p>
+    <div class="container_body--xs">
+      <form method="post" class="form">
+      <h2 class="form_title">ご登録のメールアドレスを入力してください</h2>
+
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            メールアドレス
+          </div>
+          <input type="text" name="email" class="form_input" value="" placeholder="example@test.com">
+        </label>
+        <p class="form_errMsg"></p>
+
+
+        <button type="submit" class="form_button" name="">認証コードを送る</button>
+
+        <p class="form_notion"><a href="login.php" class="link"><span style="font-size:18px;">&gt</span> ログインフォームへ戻る</a></p>
+        <p class="form_notion"><a href="register.php" class="link"><span style="font-size:18px;">&gt</span> ご登録がお済みでない方はこちら</a></p>
+      </form>
+    </div>
+  </div>
+</main>
+
+<!--　共通フッター呼び出し　-->
+<?php require('footer.php'); ?>
