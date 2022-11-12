ad42fd67315755971ce3e1a2f0263c246e395a2f
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-11-12 16:44:12 +0900

    register.php レスポンシブ対応

--- register.php
+++ register.php
@@ -11,44 +11,42 @@ require('header.php');
 <main class="page-wrapper">
   <div class="container--baseColor">
     <h1 class="container_title">会員登録（無料）</h1>
-    <div class="container_body">
-      <div class="form">
-        <h2 class="form_title">ご登録フォーム</h2>
-        <form method="post" class="">
-          <label>
-            <div class="form_name">
-              <span class="form_label form_label--required">必須</span>
-              メールアドレス
-            </div>
-            <input type="text" name="email" class="form_input" value="" placeholder="example@test.com">
-          </label>
-          <p class="form_errMsg"></p>
+    <div class="container_body--xs">
+      <form method="post" class="form">
+      <h2 class="form_title">ご登録フォーム</h2>
 
-          <label>
-            <div class="form_name">
-              <span class="form_label form_label--required">必須</span>
-              パスワード
-              <span class="smallfont">（半角英数字6文字以上）</span>
-            </div>
-            <input type="password" name="pass" class="form_input" value="">
-          </label>
-          <p class="form_errMsg"></p>
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            メールアドレス
+          </div>
+          <input type="text" name="email" class="form_input" value="" placeholder="example@test.com">
+        </label>
+        <p class="form_errMsg"></p>
 
-          <label>
-            <div class="form_name">
-              <span class="form_label form_label--required">必須</span>
-              パスワード再入力
-            </div>
-            <input type="password" name="pass_re" class="form_input" value="">
-          </label>
-          <p class="form_errMsg"></p>
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            パスワード
+            <span class="smallfont">（半角英数字6文字以上）</span>
+          </div>
+          <input type="password" name="pass" class="form_input" value="">
+        </label>
+        <p class="form_errMsg"></p>
 
-          <button type="submit" class="form_button" name="">登録する</button>
+        <label>
+          <div class="form_name">
+            <span class="form_label form_label--required">必須</span>
+            パスワード再入力
+          </div>
+          <input type="password" name="pass_re" class="form_input" value="">
+        </label>
+        <p class="form_errMsg"></p>
 
-          <p class="form_notion"><a href="login.php" class="link"><span style="font-size:18px;">&gt</span> 登録済みの方はこちら</a></p>
-        </form>
-      </div>
+        <button type="submit" class="form_button" name="">登録する</button>
 
+        <p class="form_notion"><a href="login.php" class="link"><span style="font-size:18px;">&gt</span> 登録済みの方はこちら</a></p>
+      </form>
     </div>
   </div>
 </main>
