42bf91bee6c62624eb96380c716b0c01aa47c3e7
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-12-10 11:36:07 +0900

    add:prefectures.php新規作成
    各都道府県専用のページを新規作成

---
+++ prefectures.php
@@ -0,0 +1,312 @@
+<?php
+$page_title = '熊本県';
+require ('head.php');
+require ('header.php');
+ ?>
+
+    <main>
+      <section id="firstView" class="firstView js-header-change-target">
+        <div class="firstView_container">
+          <h1 class="firstView_title">熊本</h1>
+        </div>
+      </section>
+
+      <section id="information" class="container--subColor information">
+        <h2 class="container_title">旅の情報</h2>
+        <div class="container_body">
+          <div class="list--wish">
+            <h3 class="list_title">みんなの興味関心 in 熊本県</h3>
+            <ul class="list_body--wish">
+              <li class="panel--wish">
+                <div class="userInfo">
+                  <img src="img/プロフィールアイコン：有色.jpeg" class="userInfo_img" alt="">
+                  <p class="userInfo_name">ユーザーネーム</p>
+                </div>
+                <table class="panel_table">
+                  <tr class="panel_tableElm">
+                    <th>WHERE</th>
+                    <td>熊本城</td>
+                  </tr>
+                  <tr class="panel_tableElm">
+                    <th>WHAT</th>
+                    <td>雪の熊本城を撮りたい！</td>
+                  </tr>
+                </table>
+                <i></i>
+              </li>
+
+              <li class="panel--wish">
+                <div class="userInfo">
+                  <img src="img/プロフィールアイコン：有色.jpeg" class="userInfo_img" alt="">
+                  <p class="userInfo_name">ユーザーネーム</p>
+                </div>
+                <table class="panel_table">
+                  <tr class="panel_tableElm">
+                    <th>WHERE</th>
+                    <td>熊本城</td>
+                  </tr>
+                  <tr class="panel_tableElm">
+                    <th>WHAT</th>
+                    <td>雪の熊本城を撮りたい！</td>
+                  </tr>
+                </table>
+              </li>
+
+              <li class="panel--wish">
+                <div class="userInfo">
+                  <img src="img/プロフィールアイコン：有色.jpeg" class="userInfo_img" alt="">
+                  <p class="userInfo_name">ユーザーネーム</p>
+                </div>
+                <table class="panel_table">
+                  <tr class="panel_tableElm">
+                    <th>WHERE</th>
+                    <td>熊本城</td>
+                  </tr>
+                  <tr class="panel_tableElm">
+                    <th>WHAT</th>
+                    <td>雪の熊本城を撮りたい！</td>
+                  </tr>
+                </table>
+              </li>
+
+
+              <li class="panel--wish">
+                <div class="userInfo">
+                  <img src="img/プロフィールアイコン：有色.jpeg" class="userInfo_img" alt="">
+                  <p class="userInfo_name">ユーザーネーム</p>
+                </div>
+                <table class="panel_table">
+                  <tr class="panel_tableElm">
+                    <th>WHERE</th>
+                    <td>熊本城</td>
+                  </tr>
+                  <tr class="panel_tableElm">
+                    <th>WHAT</th>
+                    <td>雪の熊本城を撮りたい！</td>
+                  </tr>
+                </table>
+              </li>
+
+              <li class="panel--wish">
+                <div class="userInfo">
+                  <img src="img/プロフィールアイコン：有色.jpeg" class="userInfo_img" alt="">
+                  <p class="userInfo_name">ユーザーネーム</p>
+                </div>
+                <table class="panel_table">
+                  <tr class="panel_tableElm">
+                    <th>WHERE</th>
+                    <td>熊本城</td>
+                  </tr>
+                  <tr class="panel_tableElm">
+                    <th>WHAT</th>
+                    <td>雪の熊本城を撮りたい！</td>
+                  </tr>
+                </table>
+              </li>
+
+              <li class="panel--wish">
+                <div class="userInfo">
+                  <img src="img/プロフィールアイコン：有色.jpeg" class="userInfo_img" alt="">
+                  <p class="userInfo_name">ユーザーネーム</p>
+                </div>
+                <table class="panel_table">
+                  <tr class="panel_tableElm">
+                    <th>WHERE</th>
+                    <td>熊本城</td>
+                  </tr>
+                  <tr class="panel_tableElm">
+                    <th>WHAT</th>
+                    <td>雪の熊本城を撮りたい！</td>
+                  </tr>
+                </table>
+              </li>
+
+              <li class="panel--wish">
+                <div class="userInfo">
+                  <img src="img/プロフィールアイコン：有色.jpeg" class="userInfo_img" alt="">
+                  <p class="userInfo_name">ユーザーネーム</p>
+                </div>
+                <table class="panel_table">
+                  <tr class="panel_tableElm">
+                    <th>WHERE</th>
+                    <td>熊本城</td>
+                  </tr>
+                  <tr class="panel_tableElm">
+                    <th>WHAT</th>
+                    <td>雪の熊本城を撮りたい！</td>
+                  </tr>
+                </table>
+              </li>
+            </ul>
+          </div>
+          <div class="mapAndSns">
+            <div class="mapAndSns_googlemap" style="height:400px; width:400px;">
+              <h3>熊本県のマップ</h3>
+              <div class="" style="height:350px; width:350px; background:white;">
+
+              </div>
+            </div>
+            <div class="mapAndSns_twitter" style="height:400px; width:400px;">
+              <h3>熊本県の工事情報</h3>
+              <div class="" style="height:350px; width:350px; background:white;">
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+      <div class="imgSlider">
+        <ul class="imgSlider_list">
+          <li class="imgSlider_item"><img src="img/noimage.png" class="imgSlider_img" alt=""></li>
+          <li class="imgSlider_item"><img src="img/noimage.png" class="imgSlider_img" alt=""></li>
+          <li class="imgSlider_item"><img src="img/noimage.png" class="imgSlider_img" alt=""></li>
+          <li class="imgSlider_item"><img src="img/noimage.png" class="imgSlider_img" alt=""></li>
+          <li class="imgSlider_item"><img src="img/noimage.png" class="imgSlider_img" alt=""></li>
+          <li class="imgSlider_item"><img src="img/noimage.png" class="imgSlider_img" alt=""></li>
+          <li class="imgSlider_item"><img src="img/noimage.png" class="imgSlider_img" alt=""></li>
+          <li class="imgSlider_item"><img src="img/noimage.png" class="imgSlider_img" alt=""></li>
+        </ul>
+
+      </div>
+      <section id="note" class="container--note">
+        <h2 class="container_title">熊本県を訪れた人のノート</h2>
+        <div class="container_body--l">
+          <div class="list--note">
+            <ul class="list_body">
+              <li class="panel panel--note">
+                <img src="img/IMG_5131.JPG" class="panel_thumbnail" alt="">
+                <div class="panel_info">
+                  <h3 class="panel_title">朝5時に家を出てから、18時間での熊本訪問</h3>
+                  <div class="userInfo">
+                    <img src="img/プロフィールアイコン：有色.jpeg" class="userInfo_img" alt="">
+                    <p class="userInfo_name">ユーザーネーム</p>
+                  </div>
+                  <p class="panel_postDay">2002/08/06投稿</p>
+                  <div class="iconBox">
+                    <i></i>
+                    <span></span>
+                    <i></i>
+                    <span></span>
+                  </div>
+                </div>
+              </li>
+
+              <li class="panel panel--note">
+                <img src="img/6241759280_IMG_3459.JPG" class="panel_thumbnail" alt="">
+                <div class="panel_info">
+                  <h3 class="panel_title">熊本の、城とラーメンと人情と</h3>
+                  <div class="userInfo">
+                    <img src="img/noimage.png" class="userInfo_img" alt="">
+                    <p class="userInfo_name">ユーザーネーム</p>
+                  </div>
+                  <p class="panel_postDay">2002/08/06投稿</p>
+                  <div class="iconBox">
+                    <i></i>
+                    <span></span>
+                    <i></i>
+                    <span></span>
+                  </div>
+                </div>
+              </li>
+
+              <li class="panel panel--note">
+                <img src="img/6176658528_IMG_4498.JPG" class="panel_thumbnail" alt="">
+                <div class="panel_info">
+                  <h3 class="panel_title">いつか行ってみたいと思っていた熊本に行ってきました！</h3>
+                  <div class="userInfo">
+                    <img src="img/プロフィール.JPG" class="userInfo_img" alt="">
+                    <p class="userInfo_name">ユーザーネーム</p>
+                  </div>
+                  <p class="panel_postDay">2002/08/06投稿</p>
+                  <div class="iconBox">
+                    <i></i>
+                    <span></span>
+                    <i></i>
+                    <span></span>
+                  </div>
+                </div>
+              </li>
+
+              <li class="panel panel--note">
+                <img src="img/IMG_5506.JPG" class="panel_thumbnail" alt="">
+                <div class="panel_info">
+                  <h3 class="panel_title">タイトル</h3>
+                  <div class="userInfo">
+                    <img src="img/noimage.png" class="userInfo_img" alt="">
+                    <p class="userInfo_name">ユーザーネーム</p>
+                  </div>
+                  <p class="panel_postDay">2002/08/06投稿</p>
+                  <div class="iconBox">
+                    <i></i>
+                    <span></span>
+                    <i></i>
+                    <span></span>
+                  </div>
+                </div>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section id="changeDestination" class="container">
+        <h2 class="container_title">行き先を変える</h2>
+        <div class="container_body--l">
+          <div class="list--destination">
+            <!-- <h3 class="list_title">関東</h3> -->
+            <div class="list_body--destination">
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">東京</span>
+              </a>
+
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">千葉</span>
+              </a>
+
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">神奈川</span>
+              </a>
+
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">埼玉</span>
+              </a>
+
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">静岡</span>
+              </a>
+
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">愛知</span>
+              </a>
+
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">京都</span>
+              </a>
+
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">大阪</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+<?php require('footer.php'); ?>
