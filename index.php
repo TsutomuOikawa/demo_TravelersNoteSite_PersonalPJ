1e708bc32b46ad90da68198ba92a815853f8fe6e
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-12-10 18:53:18 +0900

    add:サーチのコンテンツを追加
    空だったコンテンツを追加
    prefecturesで作成したノート一覧と都道府県一覧の使い回し

--- index.php
+++ index.php
@@ -79,19 +79,147 @@ require ('header.php');
       <section id="search" class="container--baseColor">
         <h2 class="container_title">旅先を探す</h2>
         <div class="container_body--l">
-          <div class="list">
-            <h3 class="list_title">みんなのノートから探す</h3>
-            <div class="list_body" style="height:300px; background:grey;">
+          <h3 class="list_title">人気のノートから探す</h3>
+          <div class="list--note">
+            <ul class="list_body--note">
+              <li class="panel--note">
+                <img src="img/IMG_5131.JPG" class="panel_thumbnail" alt="">
+                <div class="panel_info">
+                  <h3 class="panel_title">朝5時に家を出てから、18時間での熊本訪問</h3>
+                  <div class="userInfo">
+                    <img src="img/プロフィールアイコン：有色.jpeg" class="userInfo_img" alt="">
+                    <p class="userInfo_name">ユーザーネーム</p>
+                  </div>
+                  <p class="panel_postDay">2002/08/06投稿</p>
+                  <div class="iconBox">
+                    <i></i>
+                    <span></span>
+                    <i></i>
+                    <span></span>
+                  </div>
+                </div>
+              </li>
 
-            </div>
+              <li class="panel--note">
+                <img src="img/6241759280_IMG_3459.JPG" class="panel_thumbnail" alt="">
+                <div class="panel_info">
+                  <h3 class="panel_title">熊本の、城とラーメンと人情と</h3>
+                  <div class="userInfo">
+                    <img src="img/noimage.png" class="userInfo_img" alt="">
+                    <p class="userInfo_name">ユーザーネーム</p>
+                  </div>
+                  <p class="panel_postDay">2002/08/06投稿</p>
+                  <div class="iconBox">
+                    <i></i>
+                    <span></span>
+                    <i></i>
+                    <span></span>
+                  </div>
+                </div>
+              </li>
+
+              <li class="panel--note">
+                <img src="img/6176658528_IMG_4498.JPG" class="panel_thumbnail" alt="">
+                <div class="panel_info">
+                  <h3 class="panel_title">いつか行ってみたいと思っていた熊本に行ってきました！</h3>
+                  <div class="userInfo">
+                    <img src="img/プロフィール.JPG" class="userInfo_img" alt="">
+                    <p class="userInfo_name">ユーザーネーム</p>
+                  </div>
+                  <p class="panel_postDay">2002/08/06投稿</p>
+                  <div class="iconBox">
+                    <i></i>
+                    <span></span>
+                    <i></i>
+                    <span></span>
+                  </div>
+                </div>
+              </li>
+
+              <li class="panel--note">
+                <img src="img/IMG_5506.JPG" class="panel_thumbnail" alt="">
+                <div class="panel_info">
+                  <h3 class="panel_title">タイトル</h3>
+                  <div class="userInfo">
+                    <img src="img/noimage.png" class="userInfo_img" alt="">
+                    <p class="userInfo_name">ユーザーネーム</p>
+                  </div>
+                  <p class="panel_postDay">2002/08/06投稿</p>
+                  <div class="iconBox">
+                    <i></i>
+                    <span></span>
+                    <i></i>
+                    <span></span>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
+          <h3 class="list_title">都道府県一覧から探す</h3>
           <div class="list--destination">
-            <h3 class="list_title">都道府県一覧から探す</h3>
-            <div class="list_body" style="height:300px; background:blue;">
+            <h4 class="list_title--destination">北海道・東北</h3>
+            <div class="list_body--destination">
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">北海道</span>
+              </a>
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">青森</span>
+              </a>
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">岩手</span>
+              </a>
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">宮城</span>
+              </a>
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">秋田</span>
+              </a>
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">山形</span>
+              </a>
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">福島</span>
+              </a>
+            </div>
 
+            <h4 class="list_title--destination">南関東</h3>
+            <div class="list_body--destination">
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">東京</span>
+              </a>
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">神奈川</span>
+              </a>
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">埼玉</span>
+              </a>
+              <a class="panel--destination">
+                <img src="img/IMG_3930.JPG" class="panel_destImg" alt="">
+                <span class="panel_destCover"></span>
+                <span class="panel_destName">千葉</span>
+              </a>
             </div>
           </div>
-
         </div>
       </section>
     </main>
